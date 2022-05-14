cf7ad6b576c29d1019d4a0aef2ccb18e31c93226
Himanshu <himanshu.kumarr07@gmail.com>
2022-05-14 10:16:05 +0530

    Updates

---
+++ precache-manifest.5bce83632360bd9091ec48fe4ef73d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "963dee28907a7edc202a",
+    "url": "/textutils/static/css/main.230956bc.chunk.css"
+  },
+  {
+    "revision": "963dee28907a7edc202a",
+    "url": "/textutils/static/js/main.963dee28.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "fc2983a4bdd0c1db1f72ab35af47048e",
+    "url": "/textutils/index.html"
+  }
+];
